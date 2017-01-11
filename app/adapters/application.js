78811be50a918ca8041922733e769c2247d1d8f9
Julian M. Jones <julian@return.tc>
2017-01-11 10:27:29 -0600

    Fixing issue with JSON path

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
+    host: config.APP.rootURL,
     //Change all the model names to _.json (to match current file name structure)
     pathForType: function(modelName) {
       return Ember.String.underscore(modelName) + '.json';
